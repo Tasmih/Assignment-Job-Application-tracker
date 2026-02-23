deaafaf38be1c505fa10faff7b93107d88e30744
Tasmih <tasmih.mow20@gmail.com>
2026-02-24 01:38:17 +0600

    add no-jobs section and connect JS logic for filtering and rendering

--- script.js/script.js
+++ script.js/script.js
@@ -28,43 +28,41 @@ function calculateCount(){
 calculateCount()
 
 
-
 function toggleStyle(id){
 
-   // adding white bg for all button
     allFilterBtn.classList.add('bg-white', 'text-slate-500')
     interviewFilterBtn.classList.add('bg-white', 'text-slate-500')
     rejectFilterBtn.classList.add('bg-white', 'text-slate-500')
 
-    // if any button has blue then remove
     allFilterBtn.classList.remove('bg-blue-500', 'text-white')
     interviewFilterBtn.classList.remove('bg-blue-500', 'text-white')
     rejectFilterBtn.classList.remove('bg-blue-500', 'text-white')
 
     const selected = document.getElementById(id)
-   // console.log(selected);
-   
-   //adding bg-blue-500 for current button
+
     selected.classList.remove('bg-white','text-slate-500')
     selected.classList.add('bg-blue-500','text-white')
+
     if (id == 'interview-filter-btn'){
         allCardSection.classList.add('hidden')
         filterSection.classList.remove('hidden')
-    }else if(id == 'all-filter-btn'){
-         allCardSection.classList.remove('hidden')
-         filterSection.classList.add('hidden')
+        renderInterview()
     }
-       
-    
-
-
+    else if(id == 'reject-filter-btn'){
+        allCardSection.classList.add('hidden')
+        filterSection.classList.remove('hidden')
+        renderReject()
+    }
+    else if(id == 'all-filter-btn'){
+        allCardSection.classList.remove('hidden')
+        filterSection.classList.add('hidden')
+    }
 }
-mainContainer.addEventListener('click',function(event){
-    console.log(event.target.parentNode.parentNode);
 
- // console.log(event.target.classList.contains('interview'));
-    if(event.target.classList.contains('interview')){
-     const parenNode = event.target.parentNode.parentNode;
+mainContainer.addEventListener('click',function(event){
+
+ if(event.target.classList.contains('interview')){
+ const parenNode = event.target.parentNode.parentNode;
   console.log(parenNode);
   const companyName = parenNode.querySelector('.companyName').innerText;
   const jobName = parenNode.querySelector('.jobName').innerText;
@@ -83,14 +81,126 @@ mainContainer.addEventListener('click',function(event){
   }
   
   const companyNameExist =interviewList.find(item=> item.companyName == cardInfo.companyName)
+
  
   if (!companyNameExist){
   interviewList.push(cardInfo)
   }
-  calculateCount();
+  calculateCount()
   renderInterview ()   
-    }
+    } else if(event.target.classList.contains('reject')){
+  const parenNode = event.target.parentNode.parentNode;
+
+  const companyName = parenNode.querySelector('.companyName').innerText;
+  const jobName = parenNode.querySelector('.jobName').innerText;
+  const jobCriteria = parenNode.querySelector('.jobCriteria').innerText;
+  const statusBadge = parenNode.querySelector('.statusBadge').innerText;
+  const notes = parenNode.querySelector('.notes').innerText;
+
+  parenNode.querySelector('.statusBadge').innerText = 'Rejected'
+
+  const cardInfo ={
+    companyName,
+    jobName,
+    jobCriteria,
+    statusBadge:'Rejected',
+    notes ,
+  }
+
+  const companyNameExist = rejectList.find(
+    item => item.companyName == cardInfo.companyName
+  )
+
+  if (!companyNameExist){
+    rejectList.push(cardInfo)
+  }
+
+  calculateCount()
+  renderReject()   
+ }
+
 })
 
 
 
+
+function renderInterview (){
+    filterSection.innerHTML = ''
+
+
+    for(let interview of interviewList){
+        console.log(interview);
+    
+        let div = document.createElement('div')
+        div.className = 'jobCard bg-white p-6 flex justify-between items-start rounded-xl shadow border border-gray-200' 
+        div.innerHTML = `
+         <div class="flex-1 space-y-3">
+            <div>
+              <p class="companyName text-blue-950 font-bold">${interview.companyName}</p>
+              <p class="jobName text-slate-500">${interview.jobName}</p>
+            </div>
+
+        <p class="jobCriteria text-slate-500">${interview.jobCriteria}</p>
+
+        <button class="statusBadge bg-blue-50 py-3 px-4 text-blue-950 rounded-md inline-block">
+            ${interview.statusBadge}
+        </button>
+
+        <p class="notes text-slate-500">
+        ${interview.notes}
+
+        </p>
+
+        <div class="flex gap-3 mt-4">
+            <button class="interview bg-white py-3 px-6 text-emerald-500 rounded-md border border-green-600">
+                Interview
+            </button>
+
+            <button class="reject bg-white py-3 px-6 text-red-500 rounded-md border border-red-600">
+                Rejected
+            </button>
+            </div>
+        `
+        filterSection.appendChild(div)
+    }
+}
+function renderReject (){
+    filterSection.innerHTML = ''
+
+
+    for(let reject of rejectList){
+        console.log(reject);
+    
+        let div = document.createElement('div')
+        div.className = 'jobCard bg-white p-6 flex justify-between items-start rounded-xl shadow border border-gray-200' 
+        div.innerHTML = `
+         <div class="flex-1 space-y-3">
+            <div>
+              <p class="companyName text-blue-950 font-bold">${reject.companyName}</p>
+              <p class="jobName text-slate-500">${reject.jobName}</p>
+            </div>
+
+        <p class="jobCriteria text-slate-500">${reject.jobCriteria}</p>
+
+        <button class="statusBadge bg-blue-50 py-3 px-4 text-blue-950 rounded-md inline-block">
+            ${reject.statusBadge}
+        </button>
+
+        <p class="notes text-slate-500">
+        ${reject.notes}
+
+        </p>
+
+        <div class="flex gap-3 mt-4">
+            <button class="interview bg-white py-3 px-6 text-emerald-500 rounded-md border border-green-600">
+                Interview
+            </button>
+
+            <button class="reject bg-white py-3 px-6 text-red-500 rounded-md border border-red-600">
+                Rejected
+            </button>
+            </div>
+        `
+        filterSection.appendChild(div)
+    }
+}
