b998cd1ee3eaaa4aa92487e0a09aee9119051d98
Tasmih <tasmih.mow20@gmail.com>
2026-02-24 18:46:32 +0600

    some changes have been added

--- script.js/script.js
+++ script.js/script.js
@@ -1,24 +1,26 @@
 let interviewList = [];
 let rejectList = [];
+let currentStatus = 'all'
+
 
 let total = document.getElementById('total');
 let interviewCount = document.getElementById("interviewCount");
 let rejectCount = document.getElementById("rejectCount");
 
+//get all button
 const allFilterBtn = document.getElementById('all-filter-btn')
 const interviewFilterBtn = document.getElementById('interview-filter-btn')
 const rejectFilterBtn = document.getElementById('reject-filter-btn')
 
 
 const allCardSection = document.getElementById('allCards');
-// interviewList.push() // interview or reject button a click korle dashboard a count barbe
-const mainContainer = document.querySelector('main')
-const filterSection = document.getElementById('filtered-section')
+const mainContainer2 = document.querySelector('main');
+const filterSection = document.getElementById('filtered-section');
 
 
 
-function calculateCount(){
-    total.innerText = allCardSection.children.length // we will get 8 as we have 8 cards
+function calculateCount() {
+    total.innerText = allCardSection.children.length
     interviewCount.innerText = interviewList.length
     rejectCount.innerText = rejectList.length
 
@@ -26,114 +28,139 @@ function calculateCount(){
 calculateCount()
 
 
-function toggleStyle(id){
-
-    allFilterBtn.classList.add('bg-white', 'text-slate-500')
-    interviewFilterBtn.classList.add('bg-white', 'text-slate-500')
-    rejectFilterBtn.classList.add('bg-white', 'text-slate-500')
+function toggleStyle(id) {
 
     allFilterBtn.classList.remove('bg-blue-500', 'text-white')
     interviewFilterBtn.classList.remove('bg-blue-500', 'text-white')
     rejectFilterBtn.classList.remove('bg-blue-500', 'text-white')
 
+    allFilterBtn.classList.add('bg-white', 'text-slate-500')
+    interviewFilterBtn.classList.add('bg-white', 'text-slate-500')
+    rejectFilterBtn.classList.add('bg-white', 'text-slate-500')
+
+
+
     const selected = document.getElementById(id)
+    currentStatus = id
+    console.log(selected)
 
-    selected.classList.remove('bg-white','text-slate-500')
-    selected.classList.add('bg-blue-500','text-white')
+    selected.classList.remove('bg-white', 'text-slate-500')
+    selected.classList.add('bg-blue-500', 'text-white')
 
-    if (id == 'interview-filter-btn'){
+    if (id == 'all-filter-btn') {
+        allCardSection.classList.remove('hidden')
+        filterSection.classList.add('hidden')
+
+    }
+    else if (id == 'interview-filter-btn') {
         allCardSection.classList.add('hidden')
         filterSection.classList.remove('hidden')
         renderInterview()
     }
-    else if(id == 'reject-filter-btn'){
+    else if (id == 'reject-filter-btn') {
         allCardSection.classList.add('hidden')
         filterSection.classList.remove('hidden')
         renderReject()
-    }
-    else if(id == 'all-filter-btn'){
-        allCardSection.classList.remove('hidden')
-        filterSection.classList.add('hidden')
+    
     }
 }
 
-mainContainer.addEventListener('click', function(event){
-   
-    const parenNode = event.target.parentNode.parentNode; 
-    if(!parenNode) return;
 
-    const companyName = parenNode.querySelector('.companyName').innerText;
-    const jobName = parenNode.querySelector('.jobName').innerText;
-    const jobCriteria = parenNode.querySelector('.jobCriteria').innerText;
-    const notes = parenNode.querySelector('.notes').innerText;
+//  CLICK EVENT 
+mainContainer2.addEventListener('click', function(event){
 
+   if(event.target.classList.contains('interview')){
+
+      const parenNode = event.target.parentNode.parentNode
+
+      const companyName = parenNode.querySelector('.companyName').textContent
+      const jobName = parenNode.querySelector('.jobName').textContent
+      const jobCriteria = parenNode.querySelector('.jobCriteria').textContent
+      const notes = parenNode.querySelector('.notes').textContent
+
+      parenNode.querySelector('.statusBadge').innerText = 'Interview'
+
+      const cardInfo = {
+         companyName,
+         jobName,
+         jobCriteria,
+         statusBadge: 'Interview',
+         notes
+      }
+     const remainingCards = rejectList.filter(item =>item.companyName != companyName.trim())
+     rejectList = remainingCards
+      const companyExist = interviewList.find(items =>
+         items.companyName == cardInfo.companyName
+      )
+
+      if(!companyExist){
+         interviewList.push(cardInfo)
+      }
+
+      calculateCount()
+      renderInterview()
+   }
+
+
+   else if(event.target.classList.contains('reject')){
+
+      const parenNode = event.target.parentNode.parentNode
+
+      const companyName = parenNode.querySelector('.companyName').textContent
+      const jobName = parenNode.querySelector('.jobName').textContent
+      const jobCriteria = parenNode.querySelector('.jobCriteria').textContent
+      const notes = parenNode.querySelector('.notes').textContent
+
+      parenNode.querySelector('.statusBadge').innerText = 'Rejected'
+
+      const cardInfo = {
+         companyName,
+         jobName,
+         jobCriteria,
+         statusBadge: 'Rejected',
+         notes
+      }
+     // console.log(interviewList, cardInfo.companyName.trim())
+      const remainingCards = interviewList.filter(item =>item.companyName != companyName.trim())
+      console.log(remainingCards)
+     interviewList = remainingCards
+      const companyExist = rejectList.find(items =>
+         items.companyName == cardInfo.companyName
+      )
+
+      if(!companyExist){
+         rejectList.push(cardInfo)
+      }
+
+      if(currentStatus == "rejectFilterBtn"){
+         renderReject()
+      }
+
+      calculateCount()
+      renderReject()
     
-    if(event.target.classList.contains('interview')){
-        // remove from rejectList
-        rejectList = rejectList.filter(item => item.companyName !== companyName);
+   }
 
-        // push if missing in interviewList
-        if(!interviewList.find(item => item.companyName === companyName)){
-            interviewList.push({
-                companyName, jobName, jobCriteria, notes, statusBadge:'Interview'
-            });
-        }
+})
 
-        parenNode.querySelector('.statusBadge').innerText = 'Interview';
-        calculateCount();
-        renderInterview();
-    } 
-    // Reject button click
-    else if(event.target.classList.contains('reject')){
-      
-        interviewList = interviewList.filter(item => item.companyName !== companyName);
-
-        // push if missing in rejectList
-        if(!rejectList.find(item => item.companyName === companyName)){
-            rejectList.push({
-                companyName, jobName, jobCriteria, notes, statusBadge:'Rejected'
-            });
-        }
-
-        parenNode.querySelector('.statusBadge').innerText = 'Rejected';
-        calculateCount();
-        renderReject();
-    }
-    // Delete button click
-    else if(event.target.classList.contains('delete-btn') || event.target.parentNode.classList.contains('delete-btn')){
-        const cardNode = event.target.parentNode.parentNode; 
-        const companyNameDel = cardNode.querySelector('.companyName').innerText;
-
-        // remove from lists
-        interviewList = interviewList.filter(item => item.companyName !== companyNameDel);
-        rejectList = rejectList.filter(item => item.companyName !== companyNameDel);
-
-        
-        cardNode.remove();
-
-        
-        calculateCount();
-    }
-});
-
-
-function renderInterview (){
+function renderInterview() {
     filterSection.innerHTML = ''
+
     // if interview is empty , then show no job message
-    if (interviewList.length === 0){
+    if (interviewList.length === 0) {
         document.getElementById('no-jobs').classList.remove('hidden');
         return;
     } else {
         document.getElementById('no-jobs').classList.add('hidden');
     }
 
-    for(let interview of interviewList){
-        console.log(interview);
-    
+    for (let interview of interviewList) {
+        //console.log(interview);
+
         let div = document.createElement('div')
-        div.className = 'jobCard bg-white p-6 flex justify-between items-start rounded-xl shadow border border-gray-200' 
+        div.className = 'jobCard bg-white p-6 md:flex justify-between items-start rounded-xl shadow border border-gray-200'
         div.innerHTML = `
-         <div class="flex-1 space-y-3">
+         <div class="left flex-1 space-y-3">
             <div>
               <p class="companyName text-blue-950 font-bold">${interview.companyName}</p>
               <p class="jobName text-slate-500">${interview.jobName}</p>
@@ -163,10 +190,10 @@ function renderInterview (){
         filterSection.appendChild(div)
     }
 }
-function renderReject (){
+function renderReject() {
     filterSection.innerHTML = ''
     // if reject is empty  then show no job message
-    if(rejectList.length === 0){
+    if (rejectList.length === 0) {
         document.getElementById('no-jobs').classList.remove('hidden');
         return;
     } else {
@@ -174,11 +201,11 @@ function renderReject (){
     }
 
 
-    for(let reject of rejectList){
-        console.log(reject);
-    
+    for (let reject of rejectList) {
+       // console.log(reject);
+
         let div = document.createElement('div')
-        div.className = 'jobCard bg-white p-6 flex justify-between items-start rounded-xl shadow border border-gray-200' 
+        div.className = 'jobCard bg-white p-6 md:flex justify-between items-start rounded-md shadow border border-gray-200'
         div.innerHTML = `
          <div class="flex-1 space-y-3">
             <div>
@@ -210,3 +237,4 @@ function renderReject (){
         filterSection.appendChild(div)
     }
 }
+
